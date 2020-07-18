c1b1a7857da47a7f0795fc8f2f3d9525b47c9e82
Martin Thompson <martin@parallelpoints.com>
2020-07-18 20:19:05 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+let moveMotorZIP = Kitronik_Move_Motor.createMoveMotorZIPLED(4)
+let headlights = moveMotorZIP.range(0, 2)
+let rearlights = moveMotorZIP.range(2, 2)
 basic.forever(function () {
-	
+    if (input.lightLevel() < 20) {
+        headlights.showColor(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.White))
+        rearlights.showColor(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Red))
+    } else {
+        moveMotorZIP.clear()
+        moveMotorZIP.show()
+    }
 })
